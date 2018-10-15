a6c298eb8368440b21736f8ed8b6128e0d2cf23a
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-15 22:00:50 +0300

    share button

--- System View Controllers/ViewController.swift
+++ System View Controllers/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    @IBAction func safaryButtonTapped(_ sender: UIButton) {
+    
+    }
 
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        
+    }
 
 }
 
