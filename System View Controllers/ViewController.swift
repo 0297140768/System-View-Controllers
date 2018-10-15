2b9719a2ed96c210307e6450d248fc7da4775476
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-15 22:12:07 +0300

    safari button added

--- System View Controllers/ViewController.swift
+++ System View Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
@@ -26,7 +27,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func safaryButtonTapped(_ sender: UIButton) {
-    
+        if let url = URL(string: "https://apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
     }
 
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
