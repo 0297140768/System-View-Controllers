00fb5879e84236966acb10dc2cd5bd3a38060958
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-15 21:48:54 +0300

    Initial Commit

---
+++ System View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System View Controllers
+//
+//  Created by Татьяна on 15.10.2018.
+//  Copyright © 2018 Татьяна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
