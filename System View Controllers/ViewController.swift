eb715a1793f8f9bd7032efb501efdbdda128469f
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-15 22:54:38 +0300

    Camera button

--- System View Controllers/ViewController.swift
+++ System View Controllers/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import SafariServices
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -34,12 +35,45 @@ class ViewController: UIViewController {
     }
 
     @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
+        let alertController = UIAlertController(title: "Выберите источник изображения", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Камера", style: .default) {
+                _ in imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion:  nil)
+            }
+            alertController.addAction(cameraAction)
+
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let photoLibraryAction = UIAlertAction(title: "Фото библиотека", style: .default) {
+                _ in self.present(imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(photoLibraryAction)
+
+        }
+        alertController.popoverPresentationController?.sourceView = sender
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func emailButtonTapped(_ sender: UIButton) {
-        
+        if !MFMailComposeViewController.canSendMail() {
+            print("Нельзя послать e-mail")
+        }
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
